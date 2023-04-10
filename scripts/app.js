d380fd06e6a5db5728b5dbab3034ee4aa5844600
Sergey Trifonov <se.trif0nov@yandex.com>
2023-04-10 21:16:37 +0300

    add xhr

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
 'use strict';
 
 let habbits = [];
-const HABBIT_KEY = 'HABBIT_KEY'
+const HABBIT_KEY = 'HABBIT_KEY';
 
 // main obj 
 const page = {
-    menu: document.querySelector('.menu__list')
+    menu: document.querySelector('.menu__list'),
+    header: {
+        h1: document.querySelector('.h1'),
+        percent: document.querySelector('.progress__percent'),
+        coverBar: document.querySelector('.progress__cover-bar'),
+    },
+    body: {
+        daysContainer: document.getElementById('days'),
+        day: document.querySelector('.habbit__day')
+    }
 };
+
+// грех о сквернословие почитать
 
 // utils
 function loadData() {
@@ -17,15 +28,23 @@ function loadData() {
     }
 }
 
-function saveData() {
-    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+function requestJson (url) {
+    let req = new XMLHttpRequest();
+        req.open('GET', url, false);
+        req.setRequestHeader('Content-Type', 'application/json');
+        req.send();
+        console.log(req.status)
+        return req.response;
+
+}
+let answerDemo = requestJson('./data/demo.json');
+
+function saveData(habbits) {
+    localStorage.setItem(HABBIT_KEY, habbits);
 }
 
 // render 
 function rerenderMenu(activeHabbit) {
-    if (!activeHabbit) {
-        return;
-    }
     for (const habbit of habbits) {
         const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
         if (!existed) {
@@ -48,16 +67,46 @@ function rerenderMenu(activeHabbit) {
     }
 }
 
+function rerenderHead(activeHabbit) {
+    page.header.h1.innerText = activeHabbit.name;
+    const progress = activeHabbit.days.length / activeHabbit.target > 1 
+    ? 100 
+    : activeHabbit.days.length / activeHabbit.target * 100;
+    page.header.percent.innerText = progress.toFixed(0) + '%';
+    page.header.coverBar.setAttribute('style', `width: ${progress}%`);
+}
+
+function rerenderContent(activeHabbit) {
+    page.body.daysContainer.innerHTML = '';
+    for (const index in activeHabbit.days) {
+        const elem = document.createElement('div');
+        elem.classList.add('habbit');
+        elem.innerHTML = `
+            <div class="habbit__day">День ${Number(index) + 1}</div>
+            <div class="habbit__comment">${activeHabbit.days[index].comment}</div>
+            <button class="habbit__delete" type="button">
+                <img src="./images/delete.svg" alt="delete ${index + 1}">
+            </button>
+        `;
+        page.body.daysContainer.appendChild(elem);
+    }
+    page.body.day.innerHTML = `День ${activeHabbit.days.length + 1}`
+}
+
 function rerender(activeHabbitId) {
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
+    if(!activeHabbit) {
+        return;
+    }
     rerenderMenu(activeHabbit);
+    rerenderHead(activeHabbit);
+    rerenderContent(activeHabbit);
 }
 
 // init
 (() => {
+    saveData(answerDemo)
     loadData();
     rerender(habbits[0].id);
+    // console.log(rerender());
 })()
-
-
-
